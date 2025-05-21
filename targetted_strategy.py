b7943808d09d2481e7ad9252786d20223f8e0dc4
yedijara <ac.banyakduit88@gmail.com>
2025-05-21 12:19:12 +0700

    Add files via upload

---
+++ targetted_strategy.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from PIL import Image
+
+# Page configuration
+st.set_page_config(
+    page_title="Targetted Strategy By AFI / UFI",
+    layout="wide"
+)
+
+# Apply dark grey background
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #2e2e2e;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Title
+st.title("Targetted Strategy By AFI / UFI")
+
+# Display images vertically, full width
+image1 = Image.open("Slide1.PNG")
+st.image(image1, use_column_width=True)
+
+# Spacer
+st.markdown("<br>", unsafe_allow_html=True)
+
+image2 = Image.open("Slide2.PNG")
+st.image(image2, use_column_width=True)
